29d0700fe098bd4aa3b607cf23160448a51106ce
MikeRileyGTRI <Michael.Riley@gtri.gatech.edu>
2021-01-22 14:17:01 -0500

    Enabled CORS configuration for main controller.

--- src/main/java/edu/gatech/chai/Controller/UploadAndExportController.java
+++ src/main/java/edu/gatech/chai/Controller/UploadAndExportController.java
@@ -29,6 +29,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -62,6 +63,7 @@ import edu.gatech.chai.Submission.Repository.PatientSubmitRepository;
 import edu.gatech.chai.Submission.Service.SubmitBundleService;
 
 @Controller
+@CrossOrigin(origins = "*")
 public class UploadAndExportController {
 	@Autowired
 	MDIToFhirCMSService mappingService;
